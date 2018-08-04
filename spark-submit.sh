f6da221429483ce33087ddcff551f6421e60ea35
Saroj personal mac <sarojkmb@gmail.com>
2018-08-04 19:51:01 -0400

    reduce by phone number, print and save as text file

---
+++ spark-submit.sh
@@ -0,0 +1,8 @@
+/Users/saroj/Workspace/software/spark-2.3.1-bin-hadoop2.7/bin/spark-submit \
+--master local[1] \
+--class com.customer.data.spark.impl.CustomerDataAnalysisSparkJob \
+--conf \
+    "spark.driver.extraJavaOptions= \
+    -Dlog4j.configuration=file:/Users/saroj/Workspace/projects/customer-data-analysis/src/main/resources/log4j.xml
+    -Dcust.data.file.path=/Users/saroj/Workspace/projects/customer-data-analysis/src/test/resources/sample/sample1000.csv" \
+target/customer-data-analysis-00.01.00.00-SNAPSHOT-shaded.jar
